5e926863961694aa017c4c608c57f2feb62c75c6
Jesse Weaver <jesse.weaver@grafana.com>
2021-10-13 16:42:23 -0600

    Assume the remote renderer is old if it returns 404 to the version endpoint

--- pkg/services/rendering/rendering_test.go
+++ pkg/services/rendering/rendering_test.go
@@ -1,8 +1,11 @@
 package rendering
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/stretchr/testify/require"
 )
@@ -56,5 +59,41 @@ func TestGetUrl(t *testing.T) {
 			url := rs.getURL(path)
 			require.Equal(t, "https://localhost:3000/"+path+"&render=1", url)
 		})
+	})
+}
+
+func TestRenderingService404Behavior(t *testing.T) {
+	cfg := setting.NewCfg()
+	rs := &RenderingService{
+		Cfg: cfg,
+		log: log.New("rendering-test"),
+	}
+
+	t.Run("When renderer responds with correct version should return that version", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("{\"version\":\"2.7.1828\"}"))
+		}))
+		defer server.Close()
+
+		rs.Cfg.RendererUrl = server.URL + "/render"
+		version, err := rs.getRemotePluginVersion()
+
+		require.NoError(t, err)
+		require.Equal(t, version, "2.7.1828")
+	})
+
+	t.Run("When renderer responds with 404 should assume a valid but old version", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer server.Close()
+
+		rs.Cfg.RendererUrl = server.URL + "/render"
+		version, err := rs.getRemotePluginVersion()
+
+		require.NoError(t, err)
+		require.Equal(t, version, "1.0.0")
 	})
 }
